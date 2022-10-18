edf663909d36adaf0d3b26303872dc888707af1e
Murilo Martins <murilomartins@outlook.com>
2022-10-18 14:35:32 -0300

    :construction: User, Order and OrderStatus created

---
+++ src/main/java/com/murilomartins/dscommerce/entities/Order.java
@@ -0,0 +1,37 @@
+package com.murilomartins.dscommerce.entities;
+
+import java.time.Instant;
+
+import com.murilomartins.dscommerce.enums.OrderStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;	
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(columnDefinition = "TIMESTAMP WITHOU TIME ZONE")
+	private Instant moment;
+	private OrderStatus status;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
+	
+	public Order() {
+	}
+	
+	
+	
+		
+}
