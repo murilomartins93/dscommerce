219f2b3230ce25eda8e8f446c4a275837f299a75
Murilo Martins <murilomartins@outlook.com>
2022-12-21 15:37:23 -0300

    :construction: ProductMinDTO

--- src/main/java/com/murilomartins/dscommerce/services/ProductService.java
+++ src/main/java/com/murilomartins/dscommerce/services/ProductService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.murilomartins.dscommerce.dto.ProductDTO;
+import com.murilomartins.dscommerce.dto.ProductMinDTO;
 import com.murilomartins.dscommerce.entities.Product;
 import com.murilomartins.dscommerce.repositories.ProductRepository;
 import com.murilomartins.dscommerce.services.exceptions.DatabaseException;
@@ -34,9 +35,9 @@ public class ProductService {
 	}
 
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAll(String name, Pageable pageable) {
+	public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
 		Page<Product> result = repository.searchByName(name, pageable);
-		return result.map(x -> new ProductDTO(x));
+		return result.map(x -> new ProductMinDTO(x));
 	}
 
 	@Transactional
